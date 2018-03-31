35dd26c4cbaa851a773a4d5fe68077eb910d56d0
Stefan <djuro.95@live.com>
2018-03-31 19:58:41 +0200

    init project

---
+++ siem/src/main/java/com/example/siem/SiemApplication.java
@@ -0,0 +1,12 @@
+package com.example.siem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiemApplication.class, args);
+	}
+}
